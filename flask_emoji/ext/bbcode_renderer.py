f7e5ffc32cde8e760800965251dbbf55c67a6810
sh4nks <sh4nks7@gmail.com>
2015-02-27 17:26:51 +0100

    Added emojis

---
+++ flask_emoji/ext/bbcode_renderer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_emoji.ext.bbcode_renderer
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    The BBCode renderer
+
+    :copyright: (c) 2015 by sh4nks
+    :license: BSD, see LICENSE for more details.
+"""
